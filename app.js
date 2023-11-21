cf18acd810cca78446ffaf18f8d7816c54b6b2b9
michaelpmurphy14 <135272573+michaelpmurphy14@users.noreply.github.com>
2023-11-20 22:04:31 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,56 +1,51 @@
-let device, sensorCharacteristic, ledCharacteristic, buzzerCharacteristic, batteryCharacteristic;
+document.getElementById('connect').addEventListener('click', function() {
+    navigator.bluetooth.requestDevice({ filters: [{ services: ['19b10000-e8f2-537e-4f6c-d104768a1214'] }] })
+    .then(device => device.gatt.connect())
+    .then(server => server.getPrimaryService('19b10000-e8f2-537e-4f6c-d104768a1214'))
+    .then(service => {
+        // Get all characteristics
+        return Promise.all([
+            service.getCharacteristic('19b10001-e8f2-537e-4f6c-d104768a1214'), // Sensor Data
+            service.getCharacteristic('19b10002-e8f2-537e-4f6c-d104768a1214'), // LED Control
+            service.getCharacteristic('19b10003-e8f2-537e-4f6c-d104768a1214'), // Buzzer Control
+            service.getCharacteristic('19b10004-e8f2-537e-4f6c-d104768a1214')  // Battery Level
+        ]);
+    })
+    .then(characteristics => {
+        const [sensorChar, ledChar, buzzerChar, batteryChar] = characteristics;
 
-document.getElementById('connect').addEventListener('click', async () => {
-    try {
-        device = await navigator.bluetooth.requestDevice({
-            filters: [{ services: ['19b10000-e8f2-537e-4f6c-d104768a1214'] }]
+        // Sensor Data
+        sensorChar.startNotifications().then(_ => {
+            sensorChar.addEventListener('characteristicvaluechanged', event => {
+                const value = new TextDecoder().decode(event.target.value);
+                document.getElementById('sensorData').textContent = value;
+            });
         });
 
-        const server = await device.gatt.connect();
-        const service = await server.getPrimaryService('19b10000-e8f2-537e-4f6c-d104768a1214');
-        sensorCharacteristic = await service.getCharacteristic('19b10001-e8f2-537e-4f6c-d104768a1214');
-        ledCharacteristic = await service.getCharacteristic('19b10002-e8f2-537e-4f6c-d104768a1214');
-        buzzerCharacteristic = await service.getCharacteristic('19b10003-e8f2-537e-4f6c-d104768a1214');
-        batteryCharacteristic = await service.getCharacteristic('19b10004-e8f2-537e-4f6c-d104768a1214');
+        // LED Control
+        document.getElementById('ledColor').addEventListener('change', event => {
+            const color = event.target.value;
+            const buffer = new Uint8Array([parseInt(color.substring(1), 16)]);
+            ledChar.writeValue(buffer);
+        });
 
-        await sensorCharacteristic.startNotifications();
-        sensorCharacteristic.addEventListener('characteristicvaluechanged', handleSensorData);
+        // Buzzer Control
+        document.getElementById('buzzerOn').addEventListener('click', () => {
+            buzzerChar.writeValue(new Uint8Array([1]));
+        });
+        document.getElementById('buzzerOff').addEventListener('click', () => {
+            buzzerChar.writeValue(new Uint8Array([0]));
+        });
 
-        document.getElementById('connect').disabled = true;
-        document.getElementById('disconnect').disabled = false;
-    } catch (error) {
-        console.error('Connection failed', error);
-    }
+        // Battery Level
+        batteryChar.startNotifications().then(_ => {
+            batteryChar.addEventListener('characteristicvaluechanged', event => {
+                const value = new TextDecoder().decode(event.target.value);
+                document.getElementById('batteryLevel').textContent = value + ' V';
+            });
+        });
+    })
+    .catch(error => {
+        console.log('Connection failed!', error);
+    });
 });
-
-document.getElementById('disconnect').addEventListener('click', async () => {
-    if (!device) return;
-    await device.gatt.disconnect();
-    document.getElementById('connect').disabled = false;
-    document.getElementById('disconnect').disabled = true;
-});
-
-function handleSensorData(event) {
-    const sensorData = new TextDecoder().decode(event.target.value);
-    document.getElementById('sensorData').textContent = `Sensor Data: ${sensorData}`;
-}
-
-document.getElementById('ledRed').addEventListener('click', () => writeLedCharacteristic('red'));
-document.getElementById('ledGreen').addEventListener('click', () => writeLedCharacteristic('green'));
-document.getElementById('ledBlue').addEventListener('click', () => writeLedCharacteristic('blue'));
-document.getElementById('ledOff').addEventListener('click', () => writeLedCharacteristic('off'));
-
-document.getElementById('buzzerOn').addEventListener('click', () => writeBuzzerCharacteristic('1'));
-document.getElementById('buzzerOff').addEventListener('click', () => writeBuzzerCharacteristic('0'));
-
-async function writeLedCharacteristic(value) {
-    if (!ledCharacteristic) return;
-    const encoder = new TextEncoder();
-    await ledCharacteristic.writeValue(encoder.encode(value));
-}
-
-async function writeBuzzerCharacteristic(value) {
-    if (!buzzerCharacteristic) return;
-    const encoder = new TextEncoder();
-    await buzzerCharacteristic.writeValue(encoder.encode(value));
-}
